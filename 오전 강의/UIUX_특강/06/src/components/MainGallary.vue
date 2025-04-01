74eb8ebb2fa412cbb1b1d762b1227350b9afc82f
joojinhyeok <jjh68979@gmail.com>
2025-04-01 17:39:00 +0900

    UI/UX 특강 코드 마무리

--- 오전 강의/UIUX_특강/06/src/components/MainGallary.vue
+++ 오전 강의/UIUX_특강/06/src/components/MainGallary.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts">
+<script setup>
+import BaseAlert from './UI/BaseAlert.vue'
+import BaseBadge from './UI/BaseBadge.vue'
 import BaseButton from './UI/BaseButton.vue'
 </script>
 
@@ -8,9 +10,20 @@ import BaseButton from './UI/BaseButton.vue'
     <!-- 컴포넌트 그룹(그리드 아이템) -->
     <section class="component-card">
       <h2>버튼</h2>
-      <BaseButton class="primary">Primary</BaseButton>
-      <BaseButton class="outline">Outline</BaseButton>
-      <BaseButton class="pill">Pill</BaseButton>
+      <BaseButton variant="primary">Primary</BaseButton>
+      <BaseButton variant="outline">Outline</BaseButton>
+      <BaseButton variant="pill">Pill</BaseButton>
+    </section>
+    <section class="component-card">
+      <h2>알림창</h2>
+      <BaseAlert variant="success">Success</BaseAlert>
+      <BaseAlert variant="warning">Warning</BaseAlert>
+      <BaseAlert variant="error">Error</BaseAlert>
+    </section>
+    <section class="component-card">
+      <h2>뱃지</h2>
+      <BaseBadge variant="success">badge</BaseBadge>
+      <BaseBadge variant="number">3</BaseBadge>
     </section>
   </main>
 </template>
