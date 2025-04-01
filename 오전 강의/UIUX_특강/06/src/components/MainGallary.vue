f2a3ce4684be53fdabbef057c5d8182262a9217f
joojinhyeok <jjh68979@gmail.com>
2025-04-01 15:48:58 +0900

    코드 추가

---
+++ 오전 강의/UIUX_특강/06/src/components/MainGallary.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BaseButton from './UI/BaseButton.vue'
+</script>
+
+<template>
+  <!-- 그리드 컨테이너 -->
+  <main class="component-grid">
+    <!-- 컴포넌트 그룹(그리드 아이템) -->
+    <section class="component-card">
+      <h2>버튼</h2>
+      <BaseButton class="primary">Primary</BaseButton>
+      <BaseButton class="outline">Outline</BaseButton>
+      <BaseButton class="pill">Pill</BaseButton>
+    </section>
+  </main>
+</template>
